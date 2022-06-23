6e0a543085eb62a80191b63d782f12ce4d2f834c
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-23 10:20:10 +0300

    Created a GET request using fetch

--- src/script.js
+++ src/script.js
@@ -7,3 +7,12 @@ let searchQuery = "";
 const appId = "84bee003";
 const appKey = "329155bd64ad8ebb0f33a82ffc58e501";
 
+const fetchAPI = async () =>{
+  // Base url containing user query,appId & appKey
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&from=0&to=20`;
+
+  const response = await fetch(baseURL);
+  const data = await response.json();
+//   generateHTML(data.hits);
+  console.log(data);
+}
