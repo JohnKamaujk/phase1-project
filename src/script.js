73e589b3582e0e2f2112b7884829569e7d52b295
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-23 10:09:04 +0300

    Set up the appId & appKey

--- src/script.js
+++ src/script.js
@@ -3,3 +3,7 @@ const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector(".container");
 let searchQuery = "";
+// used edamam free api for developers to get appId & key
+const appId = "84bee003";
+const appKey = "329155bd64ad8ebb0f33a82ffc58e501";
+
