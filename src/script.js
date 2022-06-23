e1ad4b02e2448ee53e42d513cd914a81e3fb3f81
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-23 10:24:39 +0300

    Created a function that generates html for display

--- src/script.js
+++ src/script.js
@@ -7,12 +7,38 @@ let searchQuery = "";
 const appId = "84bee003";
 const appKey = "329155bd64ad8ebb0f33a82ffc58e501";
 
-const fetchAPI = async () =>{
+const fetchAPI = async () => {
   // Base url containing user query,appId & appKey
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}&from=0&to=20`;
 
   const response = await fetch(baseURL);
   const data = await response.json();
-//   generateHTML(data.hits);
+  generateHTML(data.hits);
   console.log(data);
-}
+};
+// function for generating html for display
+const generateHTML = (results) => {
+  container.classList.remove("initial");
+  let generatedHTML = "";
+  results.map((result) => {
+    generatedHTML += `
+      <div class="item">
+        <img src="${result.recipe.image}" alt="img">
+        <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="checkout-btn" target="_blank" href="${
+            result.recipe.url
+          }">Checkout Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+        <p class="item-data">Diet label: ${
+          result.recipe.dietLabels.length > 0
+            ? result.recipe.dietLabels
+            : "No Data Found"
+        }</p>
+        <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
+      </div>
+    `;
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+};
