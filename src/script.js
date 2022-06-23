009f83ebfb35a2395ce78bec8b41168bf26bde10
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-23 09:59:21 +0300

    Initialized global variables

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// created an instance of form,result div & container div
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
